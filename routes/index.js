beda3d037016569564491abfe2ad7bb50a2892c9
dannna8 <rlaeksdk823@naver.com>
2023-09-16 19:04:35 +0900

    lesson0916

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '안녕', pageName:'home.ejs' });
+  res.render('index', { title: '홈페이지', pageName:'home.ejs' }); //랜더링하는 확장자는 ejs
 });
 
 module.exports = router;
